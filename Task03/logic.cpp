6f5ce8fda2a9b6ce552eae8903f7f3245788d3c5
shakal2004 <bl4ckr0s3@mail.ru>
2025-03-28 18:15:26 +0300

    Add project files.

---
+++ Task03/logic.cpp
@@ -0,0 +1,12 @@
+﻿// Среднее арифметическое неэкстремальных элементов
+// [The arithmetic mean of non - extreme elements]
+//
+// Дан вектор натуральных чисел. Необходимо разработать 
+// функцию, которая вычисляет среднее рифметическое элементов
+// вектора без учета минимального и максимального элементов.
+
+#include "logic.h"
+
+double calculate_arithmetical_mean_of_nonextreme_elements(int* array, int size) {
+	return 0.0;
+}
