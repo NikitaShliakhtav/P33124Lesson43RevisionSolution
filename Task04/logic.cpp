7ee655d58a413c93b9691eca6f4afe8fee092486
shakal2004 <bl4ckr0s3@mail.ru>
2025-03-29 18:10:48 +0300

    4

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -6,6 +6,48 @@
 
 #include "logic.h"
 
+int get_max_index(int* array, int size) {
+	int index = 0;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (abs(array[i]) > abs(array[index])) {
+			index = i;
+		}
+
+	}
+	return index;
+}
+
+int get_min_index(int* array, int size) {
+	int index = 0;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (abs(array[i]) < abs(array[index])) {
+			index = i;
+		}
+
+	}
+	return index;
+}
+
 int sum_between_first_min_max(int* array, int size) {
-	return 0;
+	int max_index = get_max_index(array, size);
+	int min_index = get_min_index(array, size);
+	int sum = 0;
+
+	if (min_index > max_index) {
+		int t = min_index;
+		min_index = max_index;
+		max_index = t;
+
+	}
+	for (int i = min_index + 1; i < max_index; i++)
+	{
+		sum += array[i];
+	}
+
+
+	return sum;
 }
